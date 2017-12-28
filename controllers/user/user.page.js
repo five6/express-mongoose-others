266a9f8990a97a6b3d1c9f50b8f44dc397111e1e
five6 <936333511@qq.com>
2017-12-28 19:48:14 +0800

    添加login register page

--- controllers/user/user.page.js
+++ controllers/user/user.page.js
@@ -1,1 +1,16 @@
-console.log('user.page.js')
+var express = require('express');
+var app = $app;
+var router = express.Router();
+var middleware = require('../_filters/auth');
+router.get('/signup',middleware.auth, signUp);
+router.get('/signin',middleware.auth, signIn);
+
+function signUp(req, res, next){
+    console.log('sign up')
+    res.render('user/signup');
+}
+function signIn(req, res, next){
+    console.log('sign in')
+    res.render('user/signin');
+}
+$app.use('/user', router);
