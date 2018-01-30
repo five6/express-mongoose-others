063b8d3e927a988749fdb92819ce77dff79acfc8
five6 <936333511@qq.com>
2018-01-30 14:31:55 +0800

    edit

--- assets/js/user/signin.js
+++ assets/js/user/signin.js
@@ -2,20 +2,5 @@
 
 
 $(function () {
-    QC.Login({
-        btnId: "qqLoginBtn"	//插入按钮的节点id
-    });
-    WB2.anyWhere(function (W) {
-        W.widget.connectButton({
-            id: "wb_connect_btn",
-            type: "3,2",
-            callback: {
-                login: function (o) {	//登录后的回调函数
-                    console.log(o);
-                },
-                logout: function () {	//退出后的回调函数
-                }
-            }
-        });
-    });
+ 
 })
